903e2901b739e5b69cbc3a5930685ea0e8709419
ripper42 <devnull@localhost>
2008-04-18 08:32:29 +0000

    use JAVA_HOME variable in esmska.sh

--- esmska/include/esmska.sh
+++ esmska/include/esmska.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
+# Note: Set $JAVA_HOME environment variable to specify which java to use
+
+# traverse symlinks
 SCRIPT="$0"
 if [ -L "${SCRIPT}" ]; then
     SCRIPT=`readlink -f "${SCRIPT}"`
 fi
+# go to the program directory
 cd `dirname "$SCRIPT"`
 
-java -jar esmska.jar $*
+# run program
+if [ "$JAVA_HOME" ]; then
+    $JAVA_HOME/bin/java -jar esmska.jar $*
+		     else
+    java -jar esmska.jar $*
+fi
 
+# return exitcode
 EXITCODE="$?"
 exit ${EXITCODE}
