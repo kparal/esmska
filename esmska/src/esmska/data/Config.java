3c29e259ec44f3defb316430a473b17c1c83c905
ripper42 <devnull@localhost>
2008-08-05 09:53:36 +0000

    setting version to 0.11.0 beta1

--- esmska/src/esmska/data/Config.java
+++ esmska/src/esmska/data/Config.java
@@ -15,7 +15,7 @@ import java.io.Serializable;
  */
 public class Config extends Object implements Serializable {
 
-    private static final String LATEST_VERSION = "0.11.0 beta";
+    private static final String LATEST_VERSION = "0.11.0 beta1";
 
     private String version = "";
     private String senderName = "";
