e8da516748cebec9cf8f5b297be7b6cfdc4bcf7f
kamil.paral <devnull@localhost>
2009-07-19 10:22:28 +0000

    better structuring of esmska.sh

--- esmska/include/esmska.sh
+++ esmska/include/esmska.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
-# Note: Set $JAVA_HOME environment variable to specify which java to use
+# Note: Set JAVA_HOME environment variable to specify which java to use
+# Note: Modify OPTS variable below to set custom java options
+
+OPTS=""
 
 # traverse symlinks
 SCRIPT="$0"
 if [ -L "${SCRIPT}" ]; then
     SCRIPT="`readlink -f "${SCRIPT}"`"
 fi
+
 # go to the program directory
 cd "`dirname "$SCRIPT"`"
 
-# setup some additional parameters for Mac OS X
-if [ `uname` == "Darwin" ]; then
-    OPS="-Xdock:name=Esmska -Xdock:icon=icons/esmska.png -Dfile.encoding=UTF8 -Dapple.laf.useScreenMenuBar=true"
+# set some additional java options for Mac OS X
+if [ "`uname`" = "Darwin" ]; then
+    OPTS="$OPTS -Xdock:name=Esmska -Xdock:icon=icons/esmska.png -Dfile.encoding=UTF8 -Dapple.laf.useScreenMenuBar=true"
+fi
+
+# decide where to find java
+if [ "$JAVA_HOME" ]; then
+    JAVA_HOME="$JAVA_HOME/bin/"
 fi
 
 # run program
-if [ "$JAVA_HOME" ]; then
-    "$JAVA_HOME"/bin/java $OPS -jar esmska.jar $*
-                     else
-    java $OPS -jar esmska.jar $*
-fi
+"$JAVA_HOME"java $OPTS -jar esmska.jar $*
 
 # return exitcode
 EXITCODE="$?"
