a8f60599da5c7b3f3fbdd86695ab4571ed368a46
Kamil Páral <kamil.paral@gmail.com>
2011-02-06 22:52:33 +0100

    implement Icons loading cache

--- src/esmska/data/Icons.java
+++ src/esmska/data/Icons.java
@@ -4,6 +4,7 @@
  */
 package esmska.data;
 
+import java.util.HashMap;
 import javax.swing.ImageIcon;
 
 /** Class with static references to frequently used icons
@@ -11,8 +12,9 @@ import javax.swing.ImageIcon;
  * @author ripper
  */
 public class Icons {
-
     private static final String RES = "/esmska/resources/";
+    private static final HashMap<String, ImageIcon> cache = new HashMap<String, ImageIcon>();
+
     public static final ImageIcon STATUS_MESSAGE = get("message-16.png");
     public static final ImageIcon STATUS_WARNING = get("warning-16.png");
     public static final ImageIcon STATUS_INFO = get("info-16.png");
@@ -29,8 +31,14 @@ public class Icons {
     public static final ImageIcon UPDATE_SMALL = get("update-22.png");
     public static final ImageIcon UPDATE_IMPORTANT_SMALL = get("updateImportant-22.png");
 
-    /** Return an ImageIcon of a requested name */
+    /** Return an ImageIcon of a requested name loaded from default resource directory.
+     * Icons are cached for faster access. */
     public static ImageIcon get(String name) {
-        return new ImageIcon(Icons.class.getResource(RES + name));
+        ImageIcon icon = cache.get(name);
+        if (icon == null) {
+            icon = new ImageIcon(Icons.class.getResource(RES + name));
+            cache.put(name, icon);
+        }
+        return icon;
     }
 }
