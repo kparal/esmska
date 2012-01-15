69befbf0041c97b8285f6c3730f6fa5c82ef918e
Kamil Páral <kamil.paral@gmail.com>
2012-01-15 23:14:28 +0100

    fix mysterious crashes when configuring gateways
    Fixes #415.

--- src/esmska/gui/ConfigFrame.java
+++ src/esmska/gui/ConfigFrame.java
@@ -1573,6 +1573,9 @@ private void demandDeliveryReportCheckBoxActionPerformed(ActionEvent evt) {//GEN
         @Override
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
             JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            if (!(value instanceof Gateway)) {
+                return label;
+            }
             renderer.adjustLabel(label, (Gateway)value);
             return label;
         }
